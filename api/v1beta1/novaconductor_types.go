1beb9cb3e726e6d7fe9e3b78b828120a9ea3a780
Balazs Gibizer <gibi@redhat.com>
2022-09-11 17:16:30 +0200

    Define NovaConductor CRD
    This could represent both a super conductor or a cell conductor.
    
    If CellName is "cell0" then this represents a super conductor. In this case
    both the API and Cell level DB and message bus inputs are required.
    
    If CellName is something else the this conductor acts as a cell
    conductor and only the Cell level DB and message bus inputs are
    required. The API level inputs, if provided allows, the conductor to make
    upcalls (e.g. to support nova server group anti-affinity).
    
    The NovaConductor CR assumes that both the API ,if provided, and the
    Cell level DB and message bus has are accessible via the username,
    password and hostname provided. NovaConductor will run
    nova-manage db sync on the Cell DB but it will expect that the API level
    DB already has the DB schema created before the NovaCell CR is
    instantiated.

--- api/v1beta1/novaconductor_types.go
+++ api/v1beta1/novaconductor_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1beta1
 
 import (
+	condition "github.com/openstack-k8s-operators/lib-common/modules/common/condition"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,14 +29,111 @@ type NovaConductorSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of NovaConductor. Edit novaconductor_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// +kubebuilder:validation:Required
+	// CellName is the name of the Nova Cell this conductor belongs to.
+	CellName string `json:"cellName,omitempty"`
+
+	// +kubebuilder:validation:Required
+	// Secret is the name of the Secret instance containing password
+	// information for the nova-conductor service.
+	Secret string `json:"secret,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// PasswordSelectors - Field names to identify the passwords from the
+	// Secret
+	PasswordSelectors PasswordSelector `json:"passwordSelectors,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=nova
+	// ServiceUser - optional username used for this service to register in
+	// keystone
+	ServiceUser string `json:"serviceUser"`
+
+	// +kubebuilder:validation:Optional
+	// KeystoneAuthURL - the URL that the nova-conductor service can use to
+	// talk to keystone
+	// NOTE(gibi): This is made optional here to allow reusing the
+	// NovaConductorSpec struct in the Nova CR via the NovaCell CR where this
+	// information is not yet known. We could make this required via multiple
+	// options:
+	// a) create a NovaConductorTemplate that duplicates NovaConductorSpec
+	//    without this field. Use NovaCondcutorTemplate in NovaCellSpec.
+	// b) do a) but pull out a the fields to a base struct that are used in
+	//    both NovaConductorSpec and NovaCondcutorTemplate
+	// c) add a validating webhook here that runs only when NovaConductor CR is
+	//    created and does not run when Nova CR is created and make this field
+	//    required via that webhook.
+	KeystoneAuthURL string `json:"keystoneAuthURL"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=nova
+	// APIDatabaseUser - username to use when accessing the API DB
+	APIDatabaseUser string `json:"apiDatabaseUser"`
+
+	// +kubebuilder:validation:Optional
+	// APIDatabaseHostname - hostname to use when accessing the API DB. If not
+	// provided then upcalls will be disabled. This filed is Required for
+	// cell0.
+	// TODO(gibi): Add a webhook to validate cell0 constraint
+	APIDatabaseHostname string `json:"apiDatabaseHostname"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=nova
+	// APIMessageBusUser - username to use when accessing the API message bus
+	APIMessageBusUser string `json:"apiMessageBusUser"`
+
+	// +kubebuilder:validation:Optional
+	// APIMessageBusHostname - hostname to use when accessing the API message
+	// bus. If not provided then upcalls will be disabled. This filed is
+	// Required for cell0.
+	// TODO(gibi): Add a webhook to validate cell0 constraint.
+	APIMessageBusHostname string `json:"apiMessageBusHostname"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=nova
+	// CellDatabaseUser - username to use when accessing the cell DB
+	CellDatabaseUser string `json:"cellDatabaseUser"`
+
+	// +kubebuilder:validation:Optional
+	// NOTE(gibi): This should be Required, see notes in KeystoneAuthURL
+	// CellDatabaseHostname - hostname to use when accessing the cell DB
+	CellDatabaseHostname string `json:"cellDatabaseHostname"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=nova
+	// CellMessageBusUser - username to use when accessing the cell message bus
+	CellMessageBusUser string `json:"cellMessageBusUser"`
+
+	// +kubebuilder:validation:Optional
+	// NOTE(gibi): This should be Required, see notes in KeystoneAuthURL
+	// CellMessageBusHostname - hostname to use when accessing the cell message
+	// bus
+	CellMessageBusHostname string `json:"cellMessageBusHostname"`
+
+	// +kubebuilder:validation:Optional
+	// Debug - enable debug for different deploy stages. If an init container
+	// is used, it runs and the actual action pod gets started with sleep
+	// infinity
+	Debug Debug `json:"debug,omitempty"`
+
+	// +kubebuilder:validation:Required
+	// NovaServiceBase specifies the generic fields of the service
+	NovaServiceBase `json:",inline"`
 }
 
 // NovaConductorStatus defines the observed state of NovaConductor
 type NovaConductorStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// Map of hashes to track e.g. job status
+	Hash map[string]string `json:"hash,omitempty"`
+
+	// Conditions
+	Conditions condition.Conditions `json:"conditions,omitempty" optional:"true"`
+
+	// ReadyCount defines the number of replicas ready from nova-conductor
+	ReadyCount int32 `json:"readyCount,omitempty"`
 }
 
 //+kubebuilder:object:root=true
