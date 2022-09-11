bdd0def1bc4d15a8135aaea4a8d2757d6a9f82a7
Balazs Gibizer <gibi@redhat.com>
2022-09-11 16:04:17 +0200

    Empty NovaAPI CRD
    Generated with:
    
        $ operator-sdk create api \
          --group nova \
          --version v1beta1 \
          --kind NovaAPI \
          --resource \
          --controller

---
+++ api/v1beta1/novaapi_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// NovaAPISpec defines the desired state of NovaAPI
+type NovaAPISpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of NovaAPI. Edit novaapi_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// NovaAPIStatus defines the observed state of NovaAPI
+type NovaAPIStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// NovaAPI is the Schema for the novaapis API
+type NovaAPI struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NovaAPISpec   `json:"spec,omitempty"`
+	Status NovaAPIStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// NovaAPIList contains a list of NovaAPI
+type NovaAPIList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []NovaAPI `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&NovaAPI{}, &NovaAPIList{})
+}
