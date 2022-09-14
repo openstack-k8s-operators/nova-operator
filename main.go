5537b62af5555c4127a66fbe61593a4884a2b766
Balazs Gibizer <gibi@redhat.com>
2022-09-14 11:40:07 +0200

    Empty NovaMetadata CRD

--- main.go
+++ main.go
@@ -110,6 +110,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "NovaConductor")
 		os.Exit(1)
 	}
+	if err = (&controllers.NovaMetadataReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "NovaMetadata")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
