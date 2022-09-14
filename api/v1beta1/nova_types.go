28a3175b992f3cb3d71fee9a42f941f221abc2d1
Balazs Gibizer <gibi@redhat.com>
2022-09-14 11:26:42 +0200

    Define Nova CRD
    This is an CRD to define a whole nova control plane deployment.
    
    Responsibilities:
    * It will create the API database with a user and password in the
      selected DB service. But it won't run nova-manage db sync to apply
      a schema to that database. But it will pass the hostname, user, and
      password to the service CRs needing API DB access.
    
    * It will create the top level message bus access by creating a user
      with a password in the selected message bus service. Then it will
      pass the hostname, user, and password to that message bus
      service to each nova service CR needs it.
    
    * It will create the NovaAPI, NovaScheduler and if requested the
      NovaMetadata CR based on the templates.
    
    * For all NovaCell CRs in the Cells it will:
    
      * create the Cell database with a user and password in the selected DB
        service. But it won't run nova-manage db sync to apply a schema to
        that database. But it will pass the Cell database hostname, user, and
        password to the Cell CR.
    
      * create the cell message bus access by creating a user with a
        password in the selected message bus service. Then pass the hostname,
        user, and password to that message bus service to the Cell CR.
    
      * create the NovaCell CR and passing the connection info to it.

--- api/v1beta1/nova_types.go
+++ api/v1beta1/nova_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1beta1
 
 import (
+	condition "github.com/openstack-k8s-operators/lib-common/modules/common/condition"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,14 +29,95 @@ type NovaSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Nova. Edit nova_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// +kubebuilder:validation:Required
+	// KeystoneServiceSelector to select the Keystone Service instance used
+	// by the Nova services to authenticate.
+	KeystoneServiceSelector map[string]string `json:"keystoneServiceSelector,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// APIDBSelector to select the DB Service instance used for the Nova API
+	// DB. If not provided the the default DB Service of the deployment will
+	// be used for the Nova API DB.
+	APIDBSelector map[string]string `json:"apiDBSelector,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// APIMessageBusSelector to select the Message Bus Service instance used
+	// by the Nova top level services to communicate. If not provided then
+	// the deployment's default Message Bus instance will be used.
+	APIMessageBusSelector map[string]string `json:"apiMessageBusSelector,omitempty"`
+
+	// +kubebuilder:validation:Required
+	// Cells is a mapping of cell names to NovaCell objects defining the cells
+	// in the deployment. The "cell0" cell is a mandatory cell in every
+	// deployment. Moreover any real deployment needs at least one additional
+	// normal cell as "cell0" cannot have any computes.
+	CellTemplates map[string]NovaCellSpec `json:"cellTemplates"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=nova
+	// ServiceUser - optional username used for this service to register in keystone
+	ServiceUser string `json:"serviceUser"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=nova
+	// APIDatabaseUser - username to use when accessing the API DB
+	APIDatabaseUser string `json:"apiDatabaseUser"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=nova
+	// APIMessageBusUser - username to use when accessing the API message bus
+	APIMessageBusUser string `json:"apiMessageBusUser"`
+
+	// +kubebuilder:validation:Required
+	// Secret is the name of the Secret instance containing password
+	// information for nova like the keystone service password and DB passwords
+	Secret string `json:"secret,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// PasswordSelectors - Selectors to identify the DB and ServiceUser
+	// passwords from the Secret
+	PasswordSelectors PasswordSelector `json:"passwordSelectors,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// Debug - enable debug for different deploy stages. If an init container
+	// is used, it runs and the actual action pod gets started with sleep
+	// infinity
+	Debug Debug `json:"debug,omitempty"`
+
+	// +kubebuilder:validation:Required
+	// APIServiceTemplate - define the nova-api service
+	APIServiceTemplate NovaAPISpec `json:"apiServiceTemplate"`
+
+	// +kubebuilder:validation:Required
+	// SchedulerServiceTemplate- define the nova-scheduler service
+	SchedulerServiceTemplate NovaSchedulerSpec `json:"schedulerServiceTemplate"`
+
+	// +kubebuilder:validation:Optional
+	// MetadataServiceTemplate - defines the metadata service that is global for the
+	// deployment serving all the cells.
+	MetadataServiceTemplate NovaMetadataSpec `json:"metadataServiceTemplate"`
 }
 
 // NovaStatus defines the observed state of Nova
 type NovaStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// Map of hashes to track e.g. job status
+	Hash map[string]string `json:"hash,omitempty"`
+
+	// Conditions
+	Conditions condition.Conditions `json:"conditions,omitempty" optional:"true"`
+
+	// APIServiceReadyCount defines the number or replicas ready from nova-api
+	APIServiceReadyCount int32 `json:"apiServiceReadyCount,omitempty"`
+
+	// SchedulerServiceReadyCount defines the number or replicas ready from nova-scheduler
+	SchedulerServiceReadyCount int32 `json:"schedulerServiceReadyCount,omitempty"`
+
+	// MetadataReadyCount defines the number of replicas ready from
+	// nova-metadata service
+	MetadataServiceReadyCount int32 `json:"metadataServiceReadyCount,omitempty"`
 }
 
 //+kubebuilder:object:root=true
