f75bf7b1938cf5db502d906e98a01a0949dbd1e7
Balazs Gibizer <gibi@redhat.com>
2022-09-18 13:31:11 +0200

    Empty NovaNoVNCProxy CRD
        $ operator-sdk create api \
                      --group nova \
                      --version v1beta1 \
                      --kind NovaNoVNCProxy \
                      --resource \
                      --controller

--- main.go
+++ main.go
@@ -117,6 +117,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "NovaMetadata")
 		os.Exit(1)
 	}
+	if err = (&controllers.NovaNoVNCProxyReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "NovaNoVNCProxy")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
