cd2cd3726088c8674eef3b26093b046ecd0d2f4e
Balazs Gibizer <gibi@redhat.com>
2022-11-21 17:46:37 +0100

    [NovaConductor]Register cell to DB
    This is a temporary solution to get cells registered to the API DB.
    Eventually we need to run a Job from Nova controller to register them,
    but temporarily we do that during the cell conductor db sync for cells
    that has API DB access anyhow.

--- templates/novaconductor/bin/dbsync.sh
+++ templates/novaconductor/bin/dbsync.sh
@@ -7,6 +7,17 @@ then
     nova-manage api_db sync
     CELL0DB=`crudini --get /etc/nova/nova.conf database connection`
     nova-manage cell_v2 map_cell0 --database_connection ${CELL0DB}
+else
+    # NOTE(gibi): Temporary hack. Register cell to the API DB. It is only
+    # doable if the cell has access to the API DB. We should do this
+    # in a Job in the Nova contorller instead.
+    API_DB=`crudini --get /etc/nova/nova.conf api_database connection`
+    if [ ! -z "API_DB" ]
+    then
+        DB=`crudini --get /etc/nova/nova.conf database connection`
+        MQ=`crudini --get /etc/nova/nova.conf DEFAULT transport_url`
+        nova-manage cell_v2 create_cell --name "${CELL_NAME}" --database_connection "${DB}" --transport-url "${MQ}" --verbose
+    fi
 fi
 
 nova-manage db sync
