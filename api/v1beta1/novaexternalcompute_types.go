440fca98f32181cdfe9ca67f26c79ca743c5a84a
Balazs Gibizer <gibi@redhat.com>
2023-02-09 13:55:16 +0100

    [Compute]Define CRD

--- api/v1beta1/novaexternalcompute_types.go
+++ api/v1beta1/novaexternalcompute_types.go
@@ -17,25 +17,66 @@ limitations under the License.
 package v1beta1
 
 import (
+	condition "github.com/openstack-k8s-operators/lib-common/modules/common/condition"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // NovaExternalComputeSpec defines the desired state of NovaExternalCompute
 type NovaExternalComputeSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of NovaExternalCompute. Edit novaexternalcompute_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=nova
+	// NovaInstance is the name of the Nova CR that represents the deployment
+	// this compute belongs to
+	NovaInstance string `json:"novaInstance"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=cell1
+	// CellName defines the name of the cell this compute should be connected to
+	CellName string `json:"cellName"`
+
+	// +kubebuilder:validation:Optional
+	// CustomServiceConfig - customize the service config using this parameter to change service defaults,
+	// or overwrite rendered information using raw OpenStack config format. The content gets added to
+	// to /etc/<service>/<service>.conf.d directory as custom.conf file.
+	CustomServiceConfig string `json:"customServiceConfig"`
+
+	// +kubebuilder:validation:Optional
+	// ConfigOverwrite - interface to overwrite default config files like e.g. logging.conf
+	// But can also be used to add additional files. Those get added to the service config dir in /etc/<service> .
+	DefaultConfigOverwrite map[string]string `json:"defaultConfigOverwrite"`
+
+	// +kubebuilder:validation:Required
+	// InventoryConfigMapName is the name of the k8s config map that contains the ansible inventory information
+	// for this node
+	InventoryConfigMapName string `json:"inventoryConfigMapName"`
+
+	// +kubebuilder:validation:Required
+	// SSHKeySecretName is the name of the k8s Secret that contains the ssh keys to access the node
+	SSHKeySecretName string `json:"sshKeySecretName"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=true
+	// Deploy true means the nova-operator is allowed to do changes on the external compute node
+	// if necessary. If set to false then the operator will only generate the pre-requisite data (e.g. config maps)
+	// but does not do any modification on the compute node itself.
+	Deploy bool `json:"deploy"`
 }
 
 // NovaExternalComputeStatus defines the observed state of NovaExternalCompute
 type NovaExternalComputeStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// Conditions
+	Conditions condition.Conditions `json:"conditions,omitempty" optional:"true"`
+
+	// NOTE(gibi): If nova-operator ever needs RPM packages to be installed to
+	// the host then we need to communicate in the Status to the
+	// dataplane-operator probably as a list of pacakge names and a list of
+	// package repositories.
 }
 
 //+kubebuilder:object:root=true
