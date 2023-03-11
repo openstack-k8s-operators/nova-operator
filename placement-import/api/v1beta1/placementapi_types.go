abb97cf905276853c7671e7f7dc1c91148186f9d
Balazs Gibizer <gibi@redhat.com>
2023-03-11 16:20:48 +0100

    Fix omitempty + default value
    Having a CRD field with omitempty tag with a kubebuilder default value
    defined can cause unexpected behavior. See https://github.com/gibizer/operator-lint/blob/main/linters/crd/C003 for details.
    
    So this patch remove the problematic definitions and bumps the
    operator-lint version to 0.3.0 that has a check to cover this.

--- placement-import/api/v1beta1/placementapi_types.go
+++ placement-import/api/v1beta1/placementapi_types.go
@@ -72,7 +72,7 @@ type PlacementAPISpec struct {
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:default={database: PlacementDatabasePassword, service: PlacementPassword}
 	// PasswordSelectors - Selectors to identify the DB and ServiceUser password from the Secret
-	PasswordSelectors PasswordSelector `json:"passwordSelectors,omitempty"`
+	PasswordSelectors PasswordSelector `json:"passwordSelectors"`
 
 	// +kubebuilder:validation:Optional
 	// NodeSelector to target subset of worker nodes running this service
@@ -86,14 +86,13 @@ type PlacementAPISpec struct {
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:default=false
 	// PreserveJobs - do not delete jobs after they finished e.g. to check logs
-	PreserveJobs bool `json:"preserveJobs,omitempty"`
+	PreserveJobs bool `json:"preserveJobs"`
 
 	// +kubebuilder:validation:Optional
-	// +kubebuilder:default="# add your customization here"
 	// CustomServiceConfig - customize the service config using this parameter to change service defaults,
 	// or overwrite rendered information using raw OpenStack config format. The content gets added to
 	// to /etc/<service>/<service>.conf.d directory as custom.conf file.
-	CustomServiceConfig string `json:"customServiceConfig,omitempty"`
+	CustomServiceConfig string `json:"customServiceConfig"`
 
 	// +kubebuilder:validation:Optional
 	// ConfigOverwrite - interface to overwrite default config files like e.g. logging.conf or policy.json.
@@ -150,11 +149,11 @@ type PasswordSelector struct {
 	// +kubebuilder:default="PlacementDatabasePassword"
 	// Database - Selector to get the Database user password from the Secret
 	// TODO: not used, need change in mariadb-operator
-	Database string `json:"database,omitempty"`
+	Database string `json:"database"`
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:default="PlacementPassword"
 	// Service - Selector to get the service user password from the Secret
-	Service string `json:"service,omitempty"`
+	Service string `json:"service"`
 }
 
 // PlacementAPIDebug defines the observed state of PlacementAPIDebug
@@ -162,11 +161,11 @@ type PlacementAPIDebug struct {
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:default=false
 	// DBSync enable debug
-	DBSync bool `json:"dbSync,omitempty"`
+	DBSync bool `json:"dbSync"`
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:default=false
 	// Service enable debug
-	Service bool `json:"service,omitempty"`
+	Service bool `json:"service"`
 }
 
 // PlacementAPIStatus defines the observed state of PlacementAPI
