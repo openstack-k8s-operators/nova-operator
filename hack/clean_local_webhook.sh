4af94d425def05ca98b630bc562ef9db777b2213
Andrew Bays <abays@redhat.com>
2023-05-19 18:25:07 +0000

    Add missing local webhooks

--- hack/clean_local_webhook.sh
+++ hack/clean_local_webhook.sh
@@ -3,7 +3,17 @@ set -ex
 
 oc delete validatingwebhookconfiguration/vnova.kb.io --ignore-not-found
 oc delete mutatingwebhookconfiguration/mnova.kb.io --ignore-not-found
+oc delete validatingwebhookconfiguration/vnovaapi.kb.io --ignore-not-found
+oc delete mutatingwebhookconfiguration/mnovaapi.kb.io --ignore-not-found
 oc delete validatingwebhookconfiguration/vnovacell.kb.io --ignore-not-found
 oc delete mutatingwebhookconfiguration/mnovacell.kb.io --ignore-not-found
+oc delete validatingwebhookconfiguration/vnovaconductor.kb.io --ignore-not-found
+oc delete mutatingwebhookconfiguration/mnovaconductor.kb.io --ignore-not-found
 oc delete validatingwebhookconfiguration/vnovaexternalcompute.kb.io --ignore-not-found
 oc delete mutatingwebhookconfiguration/mnovaexternalcompute.kb.io --ignore-not-found
+oc delete validatingwebhookconfiguration/vnovametadata.kb.io --ignore-not-found
+oc delete mutatingwebhookconfiguration/mnovametadata.kb.io --ignore-not-found
+oc delete validatingwebhookconfiguration/vnovanovncproxy.kb.io --ignore-not-found
+oc delete mutatingwebhookconfiguration/mnovanovncproxy.kb.io --ignore-not-found
+oc delete validatingwebhookconfiguration/vnovascheduler.kb.io --ignore-not-found
+oc delete mutatingwebhookconfiguration/mnovascheduler.kb.io --ignore-not-found
