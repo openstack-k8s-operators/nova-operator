14ca37085c3025c0d5ac855d877d40c5c597c7c0
Takashi Kajinami <tkajinam@redhat.com>
2023-07-14 15:43:38 +0900

    Ensure webhook is started
    This ensures webhook is started when controller-manage deployment
    becomes ready.

--- placement-import/main.go
+++ placement-import/main.go
@@ -115,21 +115,23 @@ func main() {
 	// Acquire environmental defaults and initialize operator defaults with them
 	placementv1.SetupDefaults()
 
+	checker := healthz.Ping
 	// Setup webhooks if requested
 	if strings.ToLower(os.Getenv("ENABLE_WEBHOOKS")) != "false" {
 		if err = (&placementv1.PlacementAPI{}).SetupWebhookWithManager(mgr); err != nil {
 			setupLog.Error(err, "unable to create webhook", "webhook", "PlacementAPI")
 			os.Exit(1)
 		}
+		checker = mgr.GetWebhookServer().StartedChecker()
 	}
 
 	//+kubebuilder:scaffold:builder
 
-	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+	if err := mgr.AddHealthzCheck("healthz", checker); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
 	}
-	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+	if err := mgr.AddReadyzCheck("readyz", checker); err != nil {
 		setupLog.Error(err, "unable to set up ready check")
 		os.Exit(1)
 	}
