51a1fc9b31f9e7fb0a2cccb215267056930fa61c
Balazs Gibizer <gibi@redhat.com>
2023-08-17 16:20:07 +0200

    Ensure webhook is started
    This ensures webhook is started when controller-manage deployment becomes ready.

--- main.go
+++ main.go
@@ -132,6 +132,7 @@ func main() {
 	// Acquire environmental defaults and initialize operator defaults with them
 	novav1beta1.SetupDefaults()
 
+	checker := healthz.Ping
 	// Setup webhooks if requested
 	if strings.ToLower(os.Getenv("ENABLE_WEBHOOKS")) != "false" {
 		if err = (&novav1beta1.Nova{}).SetupWebhookWithManager(mgr); err != nil {
@@ -162,15 +163,17 @@ func main() {
 			setupLog.Error(err, "unable to create webhook", "webhook", "NovaScheduler")
 			os.Exit(1)
 		}
+
+		checker = mgr.GetWebhookServer().StartedChecker()
 	}
 
 	//+kubebuilder:scaffold:builder
 
-	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+	if err := mgr.AddHealthzCheck("healthz", checker); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
 	}
-	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+	if err := mgr.AddReadyzCheck("readyz", checker); err != nil {
 		setupLog.Error(err, "unable to set up ready check")
 		os.Exit(1)
 	}
