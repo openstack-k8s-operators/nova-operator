ea064f9ebe8da857e39f8a7f2bd7551962e7e5a4
Balazs Gibizer <gibi@redhat.com>
2024-02-27 14:54:34 +0100

    [envtest]remove unnecessary and racy db creation
    The c1c816062d66d1c8a8af8f6b9ade671d37562d05 added MariaDBDatabase and
    Account creation to the reconfiguration tests that uses Nova CRD. The
    nova controller creates these resources automatically so the test
    should not do it. This causes a race where the test case tries to create
    and already existing db, when the conroller creates it first.

--- test/functional/nova_reconfiguration_test.go
+++ test/functional/nova_reconfiguration_test.go
@@ -24,7 +24,7 @@ import (
 
 	memcachedv1 "github.com/openstack-k8s-operators/infra-operator/apis/memcached/v1beta1"
 	condition "github.com/openstack-k8s-operators/lib-common/modules/common/condition"
-	mariadbv1 "github.com/openstack-k8s-operators/mariadb-operator/api/v1beta1"
+
 	novav1 "github.com/openstack-k8s-operators/nova-operator/api/v1beta1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -88,8 +88,6 @@ func CreateNovaWith3CellsAndEnsureReady(novaNames NovaNames) {
 	keystone.SimulateKeystoneServiceReady(novaNames.KeystoneServiceName)
 	// END of common logic with Nova multicell test
 
-	mariadb.CreateMariaDBDatabase(novaNames.APIMariaDBDatabaseName.Namespace, novaNames.APIMariaDBDatabaseName.Name, mariadbv1.MariaDBDatabaseSpec{})
-	mariadb.CreateMariaDBAccount(novaNames.APIMariaDBDatabaseName.Namespace, novaNames.APIMariaDBDatabaseName.Name, mariadbv1.MariaDBAccountSpec{})
 	mariadb.SimulateMariaDBDatabaseCompleted(novaNames.APIMariaDBDatabaseName)
 	mariadb.SimulateMariaDBAccountCompleted(novaNames.APIMariaDBDatabaseName)
 	mariadb.SimulateMariaDBDatabaseCompleted(cell0.MariaDBDatabaseName)
