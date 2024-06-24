724e3538fba22a698525d52ff87ae4adeb64ad5e
Sean Mooney <work@seanmooney.info>
2024-06-24 17:50:56 +0100

    update dhcp_domain and enabled_filters defaults
    This change maintains the tripleo default for dhcp_domain
    by overriding it to the empty string.
    
    This is requried to prevent a change in behavior for adopted
    cloud and to make teh configurable FQDN feature work as expected
    out of the box. dhcp_domain is now set to ''
    
    This change is required in both the metadata api config and the comptue
    node config. envtest coverage is added to prevent this regressing.
    
    the AggregateInstanceExtraSpecsFilter is one of the most commonly enabled
    filters, this change adds it too our default set to reflect that
    
    The existing envtest config coverage is extended to ensure this is enabled
    as well as teh other imporant filter like the numa toplogy filter.
    
    These filters are enabled to ensure correct operatorion fo feature such as
    pci passthough and servergroups by default in order to simplfy the ux for
    admins.

--- test/functional/nova_scheduler_test.go
+++ test/functional/nova_scheduler_test.go
@@ -216,6 +216,20 @@ var _ = Describe("NovaScheduler controller", func() {
 				ContainSubstring("[client]\nssl=0"))
 			extraConfigData := string(configDataMap.Data["02-nova-override.conf"])
 			Expect(extraConfigData).To(Equal("foo=bar"))
+			// ensure that sepcific non default filters we expect are present
+			// the AggregateInstanceExtraSpecsFilter is enabled by default as it is
+			// one of the most common non default filters used by customers
+			Expect(configData).Should(
+				ContainSubstring("enabled_filters = AggregateInstanceExtraSpecsFilter"))
+			//  the pci_passthrough and numa_topology filters are enabled by default to
+			//  ensure numa aware schduleing works correctly out of the box
+			Expect(configData).Should(
+				ContainSubstring("PciPassthroughFilter,NUMATopologyFilter"))
+			//  the ServerGroupAntiAffinityFilter and ServerGroupAffinityFilter are enabled
+			//  by default to ensure server group aware schduleing is supported.
+			Expect(configData).Should(
+				ContainSubstring("ServerGroupAntiAffinityFilter,ServerGroupAffinityFilter"))
+
 		})
 
 		It("stored the input hash in the Status", func() {
