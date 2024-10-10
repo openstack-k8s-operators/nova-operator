96a74ab91d39131476bfc67951c80ae8d9318a0b
Martin Schuppert <mschuppert@redhat.com>
2024-10-10 18:03:36 +0200

    Make sure to log error in findObjectsForSrc()
    It is hidden right now if there is an error in configured named
    fields and index. Lets log the error and return the current state
    of requests.
    
    With this the findObjectsForSrc() will exit with an hidden error
    like:
    
    ```
    "error": "Index with name field:.spec.ksmTls.caBundleSecretName does not exist"
    ```
    
    Signed-off-by: Martin Schuppert <mschuppert@redhat.com>

--- controllers/novaconductor_controller.go
+++ controllers/novaconductor_controller.go
@@ -646,7 +646,8 @@ func (r *NovaConductorReconciler) findObjectsForSrc(ctx context.Context, src cli
 		}
 		err := r.Client.List(ctx, crList, listOps)
 		if err != nil {
-			return []reconcile.Request{}
+			l.Error(err, fmt.Sprintf("listing %s for field: %s - %s", crList.GroupVersionKind().Kind, field, src.GetNamespace()))
+			return requests
 		}
 
 		for _, item := range crList.Items {
--- controllers/novascheduler_controller.go
+++ controllers/novascheduler_controller.go
@@ -314,7 +314,8 @@ func (r *NovaSchedulerReconciler) findObjectsForSrc(ctx context.Context, src cli
 		}
 		err := r.Client.List(ctx, crList, listOps)
 		if err != nil {
-			return []reconcile.Request{}
+			l.Error(err, fmt.Sprintf("listing %s for field: %s - %s", crList.GroupVersionKind().Kind, field, src.GetNamespace()))
+			return requests
 		}
 
 		for _, item := range crList.Items {
