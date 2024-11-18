e2caf725b1be043a9d9729ead3c5cd09ead0a3c8
Oliver Walsh <owalsh@redhat.com>
2024-11-18 15:49:02 +0000

    Pass-through empty nodeSelector to podSpecs
    There is no need drop empty nodeSelector maps, omitempty already does this

--- pkg/novaconductor/dbsync.go
+++ pkg/novaconductor/dbsync.go
@@ -94,7 +94,7 @@ func CellDBSyncJob(
 		},
 	}
 
-	if instance.Spec.NodeSelector != nil && len(*instance.Spec.NodeSelector) > 0 {
+	if instance.Spec.NodeSelector != nil {
 		job.Spec.Template.Spec.NodeSelector = *instance.Spec.NodeSelector
 	}
 
