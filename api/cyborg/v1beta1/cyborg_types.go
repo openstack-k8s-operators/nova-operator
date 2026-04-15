12d677167271039bebb926d57328632536f4b2ef
Alfredo Moralejo <amoralej@redhat.com>
2026-04-15 16:26:06 +0200

    Print Status and Message on Cyborg objects
    Similar to for CyborgAPI and CyborgConductor and other OpenStack CRDs.
    
    Signed-off-by: Alfredo Moralejo <amoralej@redhat.com>

--- api/cyborg/v1beta1/cyborg_types.go
+++ api/cyborg/v1beta1/cyborg_types.go
@@ -135,6 +135,8 @@ type CyborgStatus struct {
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[0].status",description="Status"
+// +kubebuilder:printcolumn:name="Message",type="string",JSONPath=".status.conditions[0].message",description="Message"
 
 // Cyborg is the Schema for the cyborgs API.
 type Cyborg struct {
